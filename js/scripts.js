1d45a7c88e3e05103e23a2d3e7ac65fd3f2e63d2
Malgorzata Haniszewska <gosia.haniszewska@gmail.com>
2017-11-02 13:07:58 -0700

    put user mark inside grid table

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
+var player1;
+var player2;
+var currentPlayer;
+
 function Player (name, mark) {
   this.name = name;
   this.mark = mark;
+}
+
+var playerSwitch = function() {
+  if (currentPlayer === player1) {
+    currentPlayer = player2;
+  } else {
+    currentPlayer = player1;
+  }
 }
 
 $(document).ready(function() {
@@ -12,13 +24,68 @@ $(document).ready(function() {
     var player1Mark = "X";
     var player2Mark = "O";
 
-    var player1 = new Player (player1Name, player1Mark);
-    var player2 = new Player (player2Name, player2Mark);
+    player1 = new Player (player1Name, player1Mark);
+    player2 = new Player (player2Name, player2Mark);
+    currentPlayer = player1;
 
     $("#playerNames").hide();
     $(".board").show();
     $("body h3").show();
+    $("#currentPlayer").text(currentPlayer.name);
   })
 
+  $(".row1 .column1").click(function() {
+    $(".row1 .column1").text(currentPlayer.mark);
+    $("#currentPlayer").text(currentPlayer.name);
+    playerSwitch();
+  })
+
+  $(".row1 .column2").click(function() {
+    $(".row1 .column2").text(currentPlayer.mark);
+    $("#currentPlayer").text(currentPlayer.name);
+    playerSwitch();
+  })
+
+  $(".row1 .column3").click(function() {
+    $(".row1 .column3").text(currentPlayer.mark);
+    $("#currentPlayer").text(currentPlayer.name);
+    playerSwitch();
+  })
+
+  $(".row2 .column1").click(function() {
+    $(".row2 .column1").text(currentPlayer.mark);
+    $("#currentPlayer").text(currentPlayer.name);
+    playerSwitch();
+  })
+
+  $(".row2 .column2").click(function() {
+    $(".row2 .column2").text(currentPlayer.mark);
+    $("#currentPlayer").text(currentPlayer.name);
+    playerSwitch();
+  })
+
+  $(".row2 .column3").click(function() {
+    $(".row2 .column3").text(currentPlayer.mark);
+    $("#currentPlayer").text(currentPlayer.name);
+    playerSwitch();
+  })
+
+  $(".row3 .column1").click(function() {
+    $(".row3 .column1").text(currentPlayer.mark);
+    $("#currentPlayer").text(currentPlayer.name);
+    playerSwitch();
+  })
+
+  $(".row3 .column2").click(function() {
+    $(".row3 .column2").text(currentPlayer.mark);
+    $("#currentPlayer").text(currentPlayer.name);
+    playerSwitch();
+  })
+
+  $(".row3 .column3").click(function() {
+    $(".row3 .column3").text(currentPlayer.mark);
+    $("#currentPlayer").text(currentPlayer.name);
+    playerSwitch();
+  })
 
 })

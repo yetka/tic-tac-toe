9065c7556e3c9eb885cf092ec5b8b20c9366d326
Malgorzata Haniszewska <gosia.haniszewska@gmail.com>
2017-11-02 14:05:54 -0700

    add a function to find a winner

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ var currentPlayer;
 function Player (name, mark, space) {
   this.name = name;
   this.mark = mark;
-  this.space = [];
+  this.space = "";
 }
 
 var playerSwitch = function() {
@@ -13,6 +13,19 @@ var playerSwitch = function() {
     currentPlayer = player2;
   } else {
     currentPlayer = player1;
+  }
+}
+
+var winnerCheck = function() {
+  if (currentPlayer.space === "123" ||
+      currentPlayer.space === "456" ||
+      currentPlayer.space === "789" ||
+      currentPlayer.space === "147" ||
+      currentPlayer.space === "258" ||
+      currentPlayer.space === "369" ||
+      currentPlayer.space === "159" ||
+      currentPlayer.space === "758") {
+    alert("we have a winner");
   }
 }
 
@@ -37,63 +50,72 @@ $(document).ready(function() {
 
   $(".row1 .column1").click(function() {
     $(".row1 .column1").text(currentPlayer.mark);
-    currentPlayer.space.push(1);
+    currentPlayer.space = currentPlayer.space.concat(1);
+    winnerCheck();
     playerSwitch();
     $("#currentPlayer").text(currentPlayer.name);
   })
 
   $(".row1 .column2").click(function() {
     $(".row1 .column2").text(currentPlayer.mark);
-    currentPlayer.space.push(2);
+    currentPlayer.space = currentPlayer.space.concat(2);
+    winnerCheck();
     playerSwitch();
     $("#currentPlayer").text(currentPlayer.name);
   })
 
   $(".row1 .column3").click(function() {
     $(".row1 .column3").text(currentPlayer.mark);
-    currentPlayer.space.push(3);
+    currentPlayer.space = currentPlayer.space.concat(3);
+    winnerCheck();
     playerSwitch();
     $("#currentPlayer").text(currentPlayer.name);
   })
 
   $(".row2 .column1").click(function() {
     $(".row2 .column1").text(currentPlayer.mark);
-    currentPlayer.space.push(4);
+    currentPlayer.space = currentPlayer.space.concat(4);
+    winnerCheck();
     playerSwitch();
     $("#currentPlayer").text(currentPlayer.name);
   })
 
   $(".row2 .column2").click(function() {
     $(".row2 .column2").text(currentPlayer.mark);
-    currentPlayer.space.push(5);
+    currentPlayer.space = currentPlayer.space.concat(5);
+    winnerCheck();
     playerSwitch();
     $("#currentPlayer").text(currentPlayer.name);
   })
 
   $(".row2 .column3").click(function() {
     $(".row2 .column3").text(currentPlayer.mark);
-    currentPlayer.space.push(6);
+    currentPlayer.space = currentPlayer.space.concat(6);
+    winnerCheck();
     playerSwitch();
     $("#currentPlayer").text(currentPlayer.name);
   })
 
   $(".row3 .column1").click(function() {
     $(".row3 .column1").text(currentPlayer.mark);
-    currentPlayer.space.push(7);
+    currentPlayer.space = currentPlayer.space.concat(7);
+    winnerCheck();
     playerSwitch();
     $("#currentPlayer").text(currentPlayer.name);
   })
 
   $(".row3 .column2").click(function() {
     $(".row3 .column2").text(currentPlayer.mark);
-    currentPlayer.space.push(8);
+    currentPlayer.space = currentPlayer.space.concat(8);
+    winnerCheck();
     playerSwitch();
     $("#currentPlayer").text(currentPlayer.name);
   })
 
   $(".row3 .column3").click(function() {
     $(".row3 .column3").text(currentPlayer.mark);
-    currentPlayer.space.push(9);
+    currentPlayer.space = currentPlayer.space.concat(9);
+    winnerCheck();
     playerSwitch();
     $("#currentPlayer").text(currentPlayer.name);
   })
